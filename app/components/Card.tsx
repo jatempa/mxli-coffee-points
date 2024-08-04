604be7b9b85fc0d478c87de8772b84ecc1339931
Jorge Atempa <atempaitm@gmail.com>
2024-08-03 20:23:53 -0700

    Update card styles

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,7 +1,13 @@
 export default function Card(props: any) {
+  const { children, link } = props;
   return (
     <div className="card">
-      {props.children}
+      {link ?
+        <a target="_blank" href={link} rel="noreferrer">
+          {children}
+        </a>
+        : <span>{children}</span>
+      }
     </div>
   );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,111 +19,55 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <ul className="list-container">
-        <li><Card>Cafenio</Card></li>
         <li>
-          <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/cafelanegrita"
-              rel="noreferrer"
-            >
-              Cafe la Negrita
-            </a>
+          <Card>Cafenio</Card>
+        </li>
+        <li>
+          <Card link="https://www.facebook.com/cafelanegrita">Cafe la Negrita</Card>
+        </li>
+        <li>
+          <Card link="https://www.facebook.com/revoltmxli">
+            Revolt Café & Boutique Mexicali
           </Card>
         </li>
         <li>
           <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/revoltmxli"
-              rel="noreferrer"
-            >
-              Revolt Café & Boutique Mexicali
-            </a>
+            Chianti Cafe. Deli.
           </Card>
         </li>
         <li>
-          <Card>
-            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-              Chianti Cafe. Deli.
-            </a>
-          </Card>
-        </li>
-        <li>
-          <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/zafarirestaurantebar"
-              rel="noreferrer"
-            >
-              Zafari Restaurante Bar
-            </a>
+          <Card link="https://www.facebook.com/zafarirestaurantebar">
+            Zafari Restaurante Bar
           </Card>
         </li>        
         <li>
-          <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/Uno800BD"
-              rel="noreferrer"
-            >
-              UNO 800 Brew Dispatch
-            </a>
+          <Card link="https://www.facebook.com/Uno800BD">
+            UNO 800 Brew Dispatch
           </Card>
         </li>        
         <li>
-          <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/profile.php?id=100076334313413"
-              rel="noreferrer"
-            >
-              Black Garden
-            </a>
+          <Card link="https://www.facebook.com/profile.php?id=100076334313413">
+            Black Garden
           </Card>
         </li>        
         <li>
-          <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/pages/Cafe-Tr3z/604309876613741"
-              rel="noreferrer"
-            >
-              Cafe Tr3z
-            </a>
+          <Card link="https://www.facebook.com/pages/Cafe-Tr3z/604309876613741">
+            Cafe Tr3z
           </Card>
         </li>        
         <li>
-          <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/KalinkaMexicali"
-              rel="noreferrer"
-            >
-              Kalinka Mxli
-            </a>
+          <Card link="https://www.facebook.com/KalinkaMexicali">
+            Kalinka Mxli
           </Card>
         </li>        
         <li>
-          <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/BLGranVia/"
-              rel="noreferrer"
-            >
-              Blue Luna
-            </a>
+          <Card link="https://www.facebook.com/BLGranVia/">
+            Blue Luna
           </Card>
         </li>        
         <li>
-          <Card>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/profile.php?id=100047052203544"
-              rel="noreferrer"
-            >
-              Moulú
-            </a>
+          <Card link="https://www.facebook.com/profile.php?id=100047052203544">
+            Moulú
           </Card>
         </li>        
       </ul>
