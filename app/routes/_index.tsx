14f38326e6be05cab778e8c24ca20072e074e9b6
Jorge Atempa <atempaitm@gmail.com>
2024-08-03 19:43:03 -0700

    Create a grid

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@netlify/remix-runtime";
+import Card from "~/components/Card";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,94 +11,113 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Recomendaciones</h1>
-      <ul>
-        <li>Cafenio</li>
+      <ul className="list-container">
+        <li><Card>Cafenio</Card></li>
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/cafelanegrita"
-            rel="noreferrer"
-          >
-            Cafe la Negrita
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/cafelanegrita"
+              rel="noreferrer"
+            >
+              Cafe la Negrita
+            </a>
+          </Card>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/revoltmxli"
-            rel="noreferrer"
-          >
-            Revolt Café & Boutique Mexicali
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/revoltmxli"
+              rel="noreferrer"
+            >
+              Revolt Café & Boutique Mexicali
+            </a>
+          </Card>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Chianti Cafe. Deli.
-          </a>
+          <Card>
+            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+              Chianti Cafe. Deli.
+            </a>
+          </Card>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/zafarirestaurantebar"
-            rel="noreferrer"
-          >
-            Zafari Restaurante Bar
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/zafarirestaurantebar"
+              rel="noreferrer"
+            >
+              Zafari Restaurante Bar
+            </a>
+          </Card>
         </li>        
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/Uno800BD"
-            rel="noreferrer"
-          >
-            UNO 800 Brew Dispatch
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/Uno800BD"
+              rel="noreferrer"
+            >
+              UNO 800 Brew Dispatch
+            </a>
+          </Card>
         </li>        
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/profile.php?id=100076334313413"
-            rel="noreferrer"
-          >
-            Black Garden
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/profile.php?id=100076334313413"
+              rel="noreferrer"
+            >
+              Black Garden
+            </a>
+          </Card>
         </li>        
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/pages/Cafe-Tr3z/604309876613741"
-            rel="noreferrer"
-          >
-            Cafe Tr3z
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/pages/Cafe-Tr3z/604309876613741"
+              rel="noreferrer"
+            >
+              Cafe Tr3z
+            </a>
+          </Card>
         </li>        
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/KalinkaMexicali"
-            rel="noreferrer"
-          >
-            Kalinka Mxli
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/KalinkaMexicali"
+              rel="noreferrer"
+            >
+              Kalinka Mxli
+            </a>
+          </Card>
         </li>        
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/BLGranVia/"
-            rel="noreferrer"
-          >
-            Blue Luna
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/BLGranVia/"
+              rel="noreferrer"
+            >
+              Blue Luna
+            </a>
+          </Card>
         </li>        
         <li>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/profile.php?id=100047052203544"
-            rel="noreferrer"
-          >
-            Moulú
-          </a>
+          <Card>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/profile.php?id=100047052203544"
+              rel="noreferrer"
+            >
+              Moulú
+            </a>
+          </Card>
         </li>        
       </ul>
     </div>
