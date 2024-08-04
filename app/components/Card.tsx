ea6e5b0fead231072c82ff25bc423e9c5fa8f75a
Jorge Atempa <atempaitm@gmail.com>
2024-08-03 20:05:34 -0700

    Delete 'card.css' file

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,11 +1,3 @@
-import type { LinksFunction } from "@remix-run/node";
-
-import styles from "../styles/card.css?url";
-
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles }
-];
-
 export default function Card(props: any) {
   return (
     <div className="card">
