363c9b26d0d4634b0e928d80a280e1eeda052abd
Jorge Atempa <atempaitm@gmail.com>
2024-08-09 23:33:54 -0700

    Create 'header'

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -13,13 +13,8 @@ type CardProps = {
 export default function Card(props: PropsWithChildren<CardProps>) {
   const { children, link } = props;
   return (
-    <div className="card">
-      {link ?
-        <a target="_blank" href={link} rel="noreferrer">
-          {children}
-        </a>
-        : <span>{children}</span>
-      }
-    </div>
+    <article className="card">
+      <header>{children}</header>
+    </article>
   );
 }
