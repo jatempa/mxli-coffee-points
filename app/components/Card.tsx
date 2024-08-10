445be89d06c1e1750daca4bf8d7939811cff928b
Jorge Atempa <atempaitm@gmail.com>
2024-08-09 23:56:49 -0700

    Remove link property

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -11,7 +11,7 @@ type CardProps = {
 }
 
 export default function Card(props: PropsWithChildren<CardProps>) {
-  const { children, link } = props;
+  const { children } = props;
   return (
     <article className="card">
       <header>{children}</header>
